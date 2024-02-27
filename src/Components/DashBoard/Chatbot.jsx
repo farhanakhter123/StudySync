32d4ab8e8f9373db726f2ac501b2ea51d302e936
farhanakhter123 <mdfarhanxyz@gmail.com>
2024-02-28 00:11:43 +0530

    add:initial file

--- src/Components/DashBoard/Chatbot.jsx
+++ src/Components/DashBoard/Chatbot.jsx
@@ -10,7 +10,7 @@ const Chatbot = () => {
     script.onload = () => {
       window.botpressWebChat.init({
         "composerPlaceholder": "Chat with bot",
-        "botConversationDescription": "I am Developed by Team of Study Sync",
+        "botConversationDescription": "Developed by Team of Study Sync",
         "botId": "117aca69-e25e-4676-91ac-affe253b2a2c",
         "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
         "messagingUrl": "https://messaging.botpress.cloud",
@@ -18,7 +18,7 @@ const Chatbot = () => {
         "webhookId": "5d6364d8-710e-4971-9c49-35bd16fd5328",
         "lazySocket": true,
         "themeName": "prism",
-        "botName": "Study Sync Bot",
+        "botName": "Study Sync Helper",
         "stylesheet": "https://webchat-styler-css.botpress.app/prod/a220fe8a-536e-4d5d-b810-e850c4d46fe2/v30709/style.css",
         "frontendVersion": "v1",
         "useSessionStorage": true,
